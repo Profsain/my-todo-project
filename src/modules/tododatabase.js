efeb7db62c7dbbeec12db7b3bd13fa286c10c0c2
Profsain <profsaincrown@gmail.com>
2022-06-01 14:33:58 +0100

    Add todo list structure

---
+++ src/modules/tododatabase.js
@@ -0,0 +1,30 @@
+const todoList = [
+  {
+    id: 123,
+    description: 'Wash the dishes',
+    completed: false,
+  },
+  {
+    id: 124,
+    description: 'Take breakfast',
+    completed: false,
+  },
+  {
+    id: 125,
+    description: 'Join morning session meeting',
+    completed: false,
+  },
+  {
+    id: 126,
+    description: 'Create Mobile nav-bar for todo project',
+    completed: false,
+  },
+  {
+    id: 127,
+    description: 'Complete 2 coding challenge',
+    completed: false,
+  },
+
+];
+
+export default todoList;
